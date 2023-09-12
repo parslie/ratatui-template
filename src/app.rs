1d50b5a3f4e17023c08060719968d13ea1d8777a
Viktor Holta <viktor.holta@outlook.com>
2023-09-12 08:27:12 +0200

    NEW: rename State to Status & add Data struct

--- src/app.rs
+++ src/app.rs
@@ -6,15 +6,19 @@ use crossterm::event::{self, Event, KeyModifiers, KeyCode};
 use ratatui::{prelude::*, widgets::Paragraph};
 
 #[derive(PartialEq)]
-enum State {
+enum Status {
     Idling,
     Exiting,
 }
 
-fn update(event: Event, state: &mut State) {
+pub struct Data {
+    status: Status,
+}
+
+fn update(event: Event, data: &mut Data) {
     if let Event::Key(key) = event {
         if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') {
-            *state = State::Exiting;
+            data.status = Status::Exiting;
         }
     }
 }
@@ -26,12 +30,14 @@ fn render<B: Backend>(frame: &mut Frame<B>) {
 
 pub fn run<B: Backend>(terminal: &mut Terminal<B>) -> Result<(), Box<dyn Error>> {
     let poll_duration = Duration::from_millis(500);
-    let mut state = State::Idling;
+    let mut data = Data {
+        status: Status::Idling,
+    };
 
-    while state != State::Exiting {
+    while data.status != Status::Exiting {
         terminal.draw(|frame| render(frame))?;
         if event::poll(poll_duration)? {
-            update(event::read()?, &mut state);
+            update(event::read()?, &mut data);
         }
     } 
     
